0632f81807013543e61aea2b14f58f522df574d6
费政聪 <3300949577@qq.com>
2022-06-16 19:59:38 +0800

    add wav2vec2 model

--- data_preprocess.py
+++ data_preprocess.py
@@ -23,6 +23,11 @@ def mp3_to_wav(mp3_path, wav_path):
 
 
 
+def url_to_wav_file(url): 
+    return url.split('/')[-1].split('.')[0] + '.wav'
+
+
+# generated url-label list 
 def data_preprocess(excel_path, output_path, down_load=False): 
     url_list, label_list = read_excel_data(excel_path)  
     mp3_path = os.path.join(output_path, 'original_music')
@@ -35,11 +40,90 @@ def data_preprocess(excel_path, output_path, down_load=False):
                     f.write(chunk) 
             print(url) 
     
-        mp3_to_wav(mp3_path, wav_path) 
+        mp3_to_wav(mp3_path, wav_path)  
+    return url_list, label_list 
+
+
+# project label id to text information 
+def label2text_dict_generate(excel_path): 
+    df = pd.read_excel(excel_path, engine='openpyxl') 
+    id_list = [] 
+    text_list = []
+    id_list += df['cat0_id'].values.tolist() 
+    text_list += df['cat0_name'].values.tolist() 
+    id_list += df['cat1_id'].values.tolist() 
+    text_list += df['cat1_name'].values.tolist() 
+    id_list += df['cat2_id'].values.tolist() 
+    text_list += df['cat2_name'].values.tolist() 
+    print(len(id_list), len(text_list)) 
+
+    label2text_dict = dict() 
+    for i in range(len(id_list)): 
+        if id_list[i] == 0:
+            continue 
+        label2text_dict[str(id_list[i])] = text_list[i]
+    return label2text_dict 
+
+
+def add_text_excel(label_list, label2text_dict): 
+    lines = [] 
+    for label in label_list: 
+        line = '' 
+        t_label_list = label.split(',')
+        for t_label in t_label_list: 
+            if t_label in label2text_dict.keys(): 
+                line += label2text_dict[t_label] + ',' 
+        line = line[:-1] + '\n'
+        lines.append(line)
+    
+    with open('label.txt', 'w', encoding='utf-8') as f: 
+        for line in lines: 
+            f.write(line)
+
+
+# transfor multi-label to single label  label_num = 908 
+def multi_to_single(url_list, label_list, label2text_dict, wav_path): 
+    label2num_dict = dict() 
+    num2label_dict = dict() 
+
+    i = 0 
+    for label in label2text_dict.keys(): 
+        label2num_dict[label] = i 
+        num2label_dict[i] = label 
+        i += 1 
+    
+    data = [] 
+    for i in range(len(url_list)): 
+        file_name = url_to_wav_file(url_list[i]) 
+        file_path = os.path.join(wav_path, file_name) 
+        t_label_list = label_list[i].split(',') 
+        for label in t_label_list: 
+            if label not in label2num_dict.keys():
+                continue
+            item = dict() 
+            item['audio_path'] = file_path 
+            item['label'] = label2num_dict[label] 
+            data.append(item) 
+    print('data size: ', len(data))
+    return data
+
+
+
 
 
 
 if __name__ == '__main__': 
     excel_path = './data/meituan/music.xlsx' 
+    category_excel_path = './data/meituan/category.xlsx'
     output_path = './data/meituan'
-    data_preprocess(excel_path, output_path)  
+    wav_path = './data/meituan/wav_music' 
+
+    url_list, label_list = data_preprocess(excel_path, output_path)  
+    
+    label2text_dict = label2text_dict_generate(category_excel_path) 
+
+    # add one colum about label for excel 
+    # add_text_excel(label_list, label2text_dict) 
+
+    # generate single label for model training 
+    data = multi_to_single(url_list, label_list, label2text_dict, wav_path)
--- inference.py
+++ inference.py
@@ -3,16 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
 from transformers import AutoConfig, Wav2Vec2FeatureExtractor
+import os 
+from tqdm import tqdm 
 
 from model import Wav2Vec2ForSpeechClassification
 
 import librosa
-import IPython.display as ipd
 import numpy as np
 import pandas as pd
 
+from data_preprocess import data_preprocess, url_to_wav_file
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model_name_or_path = "./ckpt"
 config = AutoConfig.from_pretrained(model_name_or_path)
 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
@@ -21,16 +24,16 @@ model =  Wav2Vec2ForSpeechClassification.from_pretrained(model_name_or_path).to(
 
 
 id2label = {
-    "0": "忧郁",
-    "1": "古典",
-    "2": "乡村",
-    "3": "迪斯科",
-    "4": "嘻哈",
-    "5": "爵士",
-    "6": "金属",
-    "7": "爆裂",
-    "8": "节奏",
-    "9": "摇滚"
+    0: "忧郁",
+    1: "古典",
+    2: "乡村",
+    3: "迪斯科",
+    4: "嘻哈",
+    5: "爵士",
+    6: "金属",
+    7: "爆裂",
+    8: "节奏",
+    9: "摇滚"
   }
 
 
@@ -43,19 +46,42 @@ def speech_file_to_array_fn(path, sampling_rate):
 
 
 def predict(path, sampling_rate):
-    speech = speech_file_to_array_fn(path, sampling_rate)
+    speech = speech_file_to_array_fn(path, sampling_rate) 
     inputs = feature_extractor(speech, sampling_rate=sampling_rate, return_tensors="pt", padding=True)
-    inputs = {key: inputs[key].to(device) for key in inputs}
+    # resize input for model inference 
+    dim_len = len(inputs['input_values'].size())
+    if dim_len > 2: 
+        inputs = {key: inputs[key].squeeze(0).to(device) for key in inputs} 
+    elif dim_len < 2: 
+        inputs = {key: inputs[key].unsqueeze(0).to(device) for key in inputs} 
+    else:
+        inputs = {key: inputs[key].to(device) for key in inputs} 
 
     with torch.no_grad():
         logits = model(**inputs).logits
 
-    scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0]
-    outputs = [{"Label": config.id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in enumerate(scores)]
-    return outputs
+    scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0] 
+    index = np.argmax(scores)
+    # outputs = [{"Label": id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in enumerate(scores)]
+    return id2label[index] + '\t' + f"{round(scores[index] * 100, 3):.1f}%" + '\n'
 
 # dataset: https://www.kaggle.com/datasets/andradaolteanu/gtzan-dataset-music-genre-classification 
 
-path = "./data/gtzan/genres_original/disco/disco.00067.wav"
-outputs = predict(path, sampling_rate)
-print(outputs)
+if __name__ == '__main__': 
+    excel_path = './data/meituan/music.xlsx' 
+    output_path = './data/meituan'
+    wav_path = './data/meituan/wav_music'
+    url_list, label_list = data_preprocess(excel_path, output_path)  
+
+    lines = []
+    for url in tqdm(url_list): 
+        wav_file = url_to_wav_file(url) 
+        wav_file_path = os.path.join(wav_path, wav_file)
+    
+        outputs = predict(wav_file_path, sampling_rate)
+        lines.append(outputs) 
+        
+
+    with open('predict.txt', 'w', encoding='utf-8') as f: 
+        for line in lines: 
+            f.write(line)
